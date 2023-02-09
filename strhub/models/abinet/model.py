15f543e5df87ae924476905821f086b25010e072
sakamaki <ryu12kojiro@gmail.com>
2023-02-09 19:11:15 +0900

    format:black

--- strhub/models/abinet/model.py
+++ strhub/models/abinet/model.py
@@ -3,15 +3,14 @@ import torch.nn as nn
 
 
 class Model(nn.Module):
-
     def __init__(self, dataset_max_length: int, null_label: int):
         super().__init__()
         self.max_length = dataset_max_length + 1  # additional stop token
         self.null_label = null_label
 
     def _get_length(self, logit, dim=-1):
-        """ Greed decoder to obtain length from logit"""
-        out = (logit.argmax(dim=-1) == self.null_label)
+        """Greed decoder to obtain length from logit"""
+        out = logit.argmax(dim=-1) == self.null_label
         abn = out.any(dim)
         out = ((out.cumsum(dim) == 1) & out).max(dim)[1]
         out = out + 1  # additional end token
@@ -27,5 +26,5 @@ class Model(nn.Module):
     @staticmethod
     def _get_location_mask(sz, device=None):
         mask = torch.eye(sz, device=device)
-        mask = mask.float().masked_fill(mask == 1, float('-inf'))
+        mask = mask.float().masked_fill(mask == 1, float("-inf"))
         return mask
--- strhub/models/modules.py
+++ strhub/models/modules.py
@@ -7,7 +7,9 @@ class BidirectionalLSTM(nn.Module):
 
     def __init__(self, input_size, hidden_size, output_size):
         super().__init__()
-        self.rnn = nn.LSTM(input_size, hidden_size, bidirectional=True, batch_first=True)
+        self.rnn = nn.LSTM(
+            input_size, hidden_size, bidirectional=True, batch_first=True
+        )
         self.linear = nn.Linear(hidden_size * 2, output_size)
 
     def forward(self, input):
@@ -15,6 +17,8 @@ class BidirectionalLSTM(nn.Module):
         input : visual feature [batch_size x T x input_size], T = num_steps.
         output : contextual feature [batch_size x T x output_size]
         """
-        recurrent, _ = self.rnn(input)  # batch_size x T x input_size -> batch_size x T x (2*hidden_size)
+        recurrent, _ = self.rnn(
+            input
+        )  # batch_size x T x input_size -> batch_size x T x (2*hidden_size)
         output = self.linear(recurrent)  # batch_size x T x output_size
         return output
